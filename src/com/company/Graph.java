06aec92b1c103aa4be8c889c20ff0fb3c921cc46
Xavier Gray <xay@carverlab.com>
2018-03-27 13:30:02 -0400

    Added a lot
    Created classes for nodes, graph, clique finder and edges
    add a dataset

---
+++ src/com/company/Graph.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Queue;
+
+public class Graph {
+    private Queue<Node> nodes;
+    private Queue<DirectedEdge> edges;
+
+    public Node getRandomNode(){
+        return new Node();
+    }
+
+}
